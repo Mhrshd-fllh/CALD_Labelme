55703d33d152efd4e7c28af43f7bda9e1d76a3c8
Mehrshad <fallah_mehrshad82@comp.iust.ac.ir>
2024-02-26 15:38:58 +0330

    Adding Label me to project and modifying some functions

--- labelme_project/main.py
+++ labelme_project/main.py
@@ -9,27 +9,38 @@ cycle_number = 0
 
 def run_labelme(input_dir, output_dir):
     # Replace 'labelme' with the actual path to the labelme executable
-    labelme_executable = 'labelme'
+    labelme_executable = os.path.join(os.getcwd(), 'labelme_project','labelme', 'labelme', 'app.py')
 
     # Example command to run LabelMe
-    command = [labelme_executable, input_dir, '-O', output_dir]
+    command = ['python', labelme_executable, input_dir, '-O', output_dir]
 
     # Run LabelMe using subprocess
     subprocess.run(command)
 
-# Example usage
-input_directory = 'path/to/images'
-output_directory = 'path/to/annotations'
 
-run_labelme(input_directory, output_directory)
+def main():
+    global cycle_number
+    input_dir = os.path.join(os.getcwd(), 'labelme_project', 'dataset', 'input_files')
+    output_dir = os.path.join(os.getcwd(), 'labelme_project', 'dataset', 'output_files')
+    yolo_input_dir = os.path.join(input_dir, 'labelme_project', 'active_learning', 'yolo')
+    yolo_output_dir = os.path.join(output_dir, 'labelme_project', 'active_learning', 'yolo')
+    labelme_input_dir = os.path.join(input_dir, 'labelme_project', 'active_learning', 'labelme')
+    labelme_output_dir = os.path.join(output_dir, 'labelme_project', 'active_learning', 'labelme')
 
+    #Labeling our train files
 
-def main(args):
-    run_labelme()
+    run_labelme(os.path.join(input_dir, 'first_files'), os.path.join(input_dir, 'train'))
+    
+    #Labeling our validation files
+
+    run_labelme(os.path.join(input_dir, 'first_files'), os.path.join(input_dir, 'validation'))
+    
     #cald_train()
     cycle_number += 1
+
     
 
 
 if __name__ == '__main__':
-    main(args)
+    import argparse
+    main()
